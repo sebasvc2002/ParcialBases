41f43d27d214fb3fa8dd0a8c3569d4a2e5325a40
Sebastian Velasco Cantu <sebasvc12345@gmail.com>
2025-10-02 19:59:21 -0600

    Modify database connection settings
    Updated database connection parameters for new server.

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,14 +2,14 @@
 
 // db_connection.php
 
-$servername = "mydigitalinnovation.com"; // Generalmente es 'localhost' en cPanel
-$username = "database_user"; // El usuario que creaste en el Paso 1
-$password = "Sebasvc1236117$"; // La contraseña que creaste en el Paso 1
-$dbname = "ventasdb"; // La base de datos que creaste en el Paso 1
+$servername = "hostname"; 
+$username = "database_user"; 
+$password = "password"; 
+$dbname = "ventasdb";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Configurar el modo de error de PDO a excepción
+    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
